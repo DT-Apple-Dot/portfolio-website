5e2cac73a14df9ea18ffc1979b952ab54990f26f
DT-Apple-Dot <NarNarismine21>
2025-04-13 02:30:33 +0630

    Finished Raw

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,103 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>Node.js</li>
+        <li>Next.js</li>
+        <li>Express</li>
+        <li>React</li>
+        <li>ReactNative</li>
+        <li>MySQL</li>
+        <li>MongoDB</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Diploma in Web Engineer</li>
+        <li>Metro IT and Japanese Language Center</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Certificate of ITPEC (IP)</li>
+        <li>Certificate of COMPUTER & LAN (Basic Networking)</li>
+      </ul>
+    ),
+  },
+];
+
+const Aboutsection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} className="rounded-3xl"/>
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full junior web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with JavaScript, React, ReactNative, Next.js, Node.js,
+            Express, MySQL, HTML, CSS, and Git. I am a quick learner and I am
+            always looking to expand my knowledge and skill set. I am a team
+            player and I am excited to work with others to create amazing
+            applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Aboutsection;
