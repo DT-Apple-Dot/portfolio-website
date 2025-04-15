7b54d2550de4c98b78623773f12b91ced6d851bf
nal27201@gmail.com <naing27201>
2025-04-15 17:58:29 +0630

    # added download cv

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div  className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
+        <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-500 to-secondary-600">
               Hello I&apos;m{" "}
@@ -25,7 +25,10 @@ const HeroSection = () => {
           </p>
           <div>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
-              <a className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <a
+                className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2"
+                href="/resume/Resume.pdf" download
+              >
                 Download CV
               </a>
             </button>
